468dac99d4fb58d8f06230bf1dc6aaedb1ea9af4
suraj-1506 <surajupadhyay442&gmail.com>
2023-12-25 20:44:27 +0530

    commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Profile from "../../assets/user.jpg";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
-import "./home.css"
+import "./home.css";
 
 const Home = () => {
   return (
@@ -14,12 +14,17 @@ const Home = () => {
             <span>I'm Suraj Upadhyay</span> Web Designer
           </h1>
           <p className="home__description">
-          Having 3.9 years of work experience as a software developer, 
-seeking a position to work in a challenging environment which 
-demands constant development of new skills and maximum 
-utilization of the existing skills for mutual growth and benefits.
+            Having 3.9 years of work experience as a software developer, seeking
+            a position to work in a challenging environment which demands
+            constant development of new skills and maximum utilization of the
+            existing skills for mutual growth and benefits.
           </p>
-          <Link to="/about" className="button">More About Me <span className="button__icon"><FaArrowRight/></span></Link>
+          <Link to="/about" className="button">
+            More About Me{" "}
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>
         </div>
       </div>
       <div className="color__block"></div>
